2b3e25b2d7f60f73491ce495d0619f93605e6bfa
notshrek <notshrek>
2024-02-12 13:40:41 +0330

    feat: add step state and fade transitions

--- app/_components/Introduction.tsx
+++ app/_components/Introduction.tsx
@@ -1,4 +1,8 @@
-export default function Introduction() {
+export default function Introduction({
+  handleStepChange,
+}: {
+  handleStepChange: Function;
+}) {
   return (
     <>
       <h1 className="text-lime-300 text-4xl font-semibold font-clashDisplay mb-8">
@@ -10,7 +14,10 @@ export default function Introduction() {
         select and relay any packet you want between any two chains easily by
         yourself, in a permissionless way and all within your browser!
       </p>
-      <button className="bg-lime-300 hover:bg-lime-200 py-4 px-8 rounded-lg font-clashDisplay font-medium text-lime-950">
+      <button
+        onClick={() => handleStepChange()}
+        className="bg-lime-300 hover:bg-lime-200 py-4 px-8 rounded-lg font-clashDisplay font-medium text-lime-950"
+      >
         START RELAYING
       </button>
     </>
--- app/_components/Main.tsx
+++ app/_components/Main.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Introduction from "./Introduction";
 
 export default function Main() {
   const mainRef = useRef<HTMLDivElement>(null);
+  const [step, setStep] = useState<number>(0);
 
   useEffect(() => {
     setTimeout(() => (mainRef.current!.style.opacity = "1"), 1750);
   }, []);
 
+  const handleStepChange = () => {
+    mainRef.current!.style.opacity = "0";
+  };
+
+  // automatically go to the next step once the content has faded out
+  const handleTransitionEnd = () => {
+    mainRef.current!.style.transitionDuration = "0.4s";
+    if (mainRef.current!.style.opacity === "0") {
+      mainRef.current!.style.opacity = "1";
+      setStep(step + 1);
+    }
+  };
+
   return (
     <div
-      className="flex flex-col items-center text-lime-100 opacity-0 duration-700"
+      className="flex flex-col items-center text-lime-100 opacity-0 duration-1000"
+      onTransitionEnd={handleTransitionEnd}
       ref={mainRef}
     >
-      <Introduction />
+      {step === 0 && <Introduction handleStepChange={handleStepChange} />}
     </div>
   );
 }
