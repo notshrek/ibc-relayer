ce3e9835dce354fe606e870271cee0670f709be5
notshrek <notshrek>
2024-02-12 21:31:28 +0330

    feat(chainselector): implement chain selection combo box

--- app/_components/ChainSelector.tsx
+++ app/_components/ChainSelector.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { Combobox } from "@headlessui/react";
+import { chains } from "chain-registry";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
 export default function ChainSelector() {
   return (
     <>
@@ -8,19 +13,94 @@ export default function ChainSelector() {
         Select the source and destination chains that the packet is meant to be
         relayed between.
       </p>
-      <ChainBox name="Source" />
-      <ChainBox name="Destination" />
+      <ChainBox side="Source" />
+      <ChainBox side="Destination" />
     </>
   );
 }
 
-function ChainBox({ name }: { name: String }) {
+function ChainBox({ side }: { side: String }) {
+  const [selectedChain, setSelectedChain] = useState<[string, string]>([
+    side === "Source" ? "celestia" : "archway-1",
+    side === "Source" ? "celestia" : "archway",
+  ]);
+  const [query, setQuery] = useState("");
+  const [showList, setShowList] = useState(false);
+
+  const chainInfo = chains.filter((chain) => {
+    return (
+      chain.chain_id === selectedChain[0] &&
+      chain.chain_name.toLowerCase() === selectedChain[1]
+    );
+  })[0];
+
+  const filteredChains =
+    query === ""
+      ? chains
+      : chains.filter((chain) => {
+          return chain.chain_name.toLowerCase().includes(query.toLowerCase());
+        });
+
   return (
-    <div className="flex flex-col gap-4 p-4 bg-white bg-opacity-5 rounded-lg mt-4">
-      <span className="font-medium">{name} Chain</span>
-      <input type="text" className="rounded-lg p-2" />
-      <button className="bg-lime-300 hover:bg-lime-200 py-2 px-36 rounded-lg text-lime-950 font-medium">
-        Connect wallet
+    <div className="flex flex-col p-4 bg-white bg-opacity-5 rounded-lg mt-2 w-full max-w-[500px]">
+      <h3 className="font-medium text-xl opacity-50 mb-4">{side} Chain</h3>
+
+      <Combobox
+        value={chainInfo.chain_name}
+        onChange={(e) => setSelectedChain([e[0], e[1]])}
+      >
+        {({ open }) => (
+          <>
+            <div className="relative flex items-center">
+              <img
+                src={chainInfo.logo_URIs?.png}
+                alt=""
+                className="absolute p-1 h-full aspect-square"
+              />
+              <Combobox.Input
+                onChange={(event) => setQuery(event.target.value)}
+                className={`w-full bg-lime-100 py-2 px-12 text-lime-950 ${open || showList ? "rounded-t-lg" : "rounded-lg"}`}
+                onFocus={() => setShowList(true)}
+                onBlur={() => {
+                  setTimeout(() => setShowList(false), 100);
+                }}
+              />
+              <ChevronUpDownIcon
+                className="absolute right-2 h-6 cursor-pointer w-auto text-lime-950"
+                onClick={() => setShowList(!showList)}
+              />
+            </div>
+            {(open || showList) && (
+              <div className="relative">
+                <Combobox.Options
+                  className="w-full grid grid-cols-2 gap-1 h-28 bg-lime-100 overflow-scroll rounded-b-lg text-lime-950 absolute z-10"
+                  static
+                >
+                  {filteredChains.map((chain) => (
+                    <Combobox.Option
+                      key={chain.chain_id + chain.chain_name}
+                      value={[chain.chain_id, chain.chain_name]}
+                      className="py-2 px-4 h-14 cursor-pointer flex items-center gap-3 font-[450] bg-lime-200 rounded-lg hover:bg-lime-300"
+                    >
+                      <img
+                        src={chain.logo_URIs?.png}
+                        alt=""
+                        className="h-8 w-auto"
+                      />
+                      <div className="overflow-hidden w-full overflow-ellipsis">
+                        {chain.chain_name}
+                      </div>
+                    </Combobox.Option>
+                  ))}
+                </Combobox.Options>
+              </div>
+            )}
+          </>
+        )}
+      </Combobox>
+
+      <button className="bg-lime-300 hover:bg-lime-200 p-2 rounded-lg text-lime-950 font-medium mt-1">
+        Connect wallet for {chainInfo.chain_name}
       </button>
     </div>
   );
