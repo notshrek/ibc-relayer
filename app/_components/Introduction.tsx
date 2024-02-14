3ea141ba85ad358c72365c9c937b8c478a2647b5
notshrek <notshrek>
2024-02-14 11:45:37 +0330

    refactor(styling): remove unnecessary font-clashdisplay from classname

--- app/_components/Introduction.tsx
+++ app/_components/Introduction.tsx
@@ -5,10 +5,10 @@ export default function Introduction({
 }) {
   return (
     <>
-      <h1 className="text-lime-300 text-4xl font-semibold font-clashDisplay mb-8 text-center">
+      <h1 className="text-lime-300 text-4xl font-semibold mb-8 text-center">
         Browser-based IBC Relayer
       </h1>
-      <p className="max-w-[750px] px-4 text-center mb-12 text-xl font-clashDisplay">
+      <p className="max-w-[750px] px-4 text-center mb-12 text-xl">
         Have an IBC packet which isn&apos;t being picked up by relayers? Or a
         timed out packet you want to revert immediately? This tool lets you
         select and relay any packet you want between any two chains easily by
@@ -16,7 +16,7 @@ export default function Introduction({
       </p>
       <button
         onClick={() => handleStepChange()}
-        className="bg-lime-300 hover:bg-lime-200 py-4 px-8 rounded-lg font-clashDisplay font-medium text-lime-950"
+        className="bg-lime-300 hover:bg-lime-200 py-4 px-8 rounded-lg font-medium text-lime-950"
       >
         START RELAYING
       </button>
